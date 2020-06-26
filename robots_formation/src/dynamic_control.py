0af248c1931e25da0c6ee318bf74dabfcac6874e
Juanesb230 <juanesb230@gmail.com>
2020-06-26 16:07:26 -0500

    shape control

--- robots_formation/src/dynamic_control.py
+++ robots_formation/src/dynamic_control.py
@@ -5,19 +5,20 @@ import math
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
-from nav_msgs.msg import Odometry
 
 uref = 0.0
 wref = 0.0
-x1 = 0.0
+x1 = 1.0
 y1 = 0.0
 theta1 = 0.0
-x2 = 0.0
-y2 = 0.0
+x2 = -1.0
+y2 = -1.0
 theta2 = 0.0
-x3 = 0.0
-y3 = 0.0
+x3 = -1.0001
+y3 = 1.0001
 theta3 = 0.0
+setpoint = np.array([[2],[2],[60*math.pi/180],[0],[0],[0]])
+setpoint_ant = setpoint
 
 def velRef(msg):
     global uref
@@ -33,7 +34,7 @@ def Odom1(msg):
     global y1
     global theta1
 
-    x1=msg.pose.pose.position.x + 1.0
+    x1=msg.pose.pose.position.x
     y1=msg.pose.pose.position.y
     rot_q=msg.pose.pose.orientation
     angles=euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
@@ -45,33 +46,59 @@ def Odom2(msg):
     global y2
     global theta2
 
-    x2=msg.pose.pose.position.x - 1.0
-    y2=msg.pose.pose.position.y - 1.0
+    x2=msg.pose.pose.position.x
+    y2=msg.pose.pose.position.y
     rot_q=msg.pose.pose.orientation
     angles=euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
     theta2=angles[2]
 
 #ODOMETRY DATA Robot 3
-def odom3(msg):
+def Odom3(msg):
     global x3
     global y3
     global theta3
 
-    x3=msg.pose.pose.position.x - 1.0
-    y3=msg.pose.pose.position.y + 1.0
+    x3=msg.pose.pose.position.x
+    y3=msg.pose.pose.position.y
     rot_q=msg.pose.pose.orientation
     angles=euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
     theta3=angles[2]
 
-def variablesFormation(x_1,y_1,x_2,y_2,x_3,y_3):
-    d1 = math.sqrt((x_1 - x_3)**2 + (y_1 - y_3)**2)
-    d2 = math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
-    d3 = math.sqrt((x_2 - x_3)**2 + (y_2 - y_3)**2)
-    beta = math.acos((d_1**2 + d_2**2 - d_3**2) / (2 * d_1 * d_2))
-    xc = (x_1 + x_2 + x_3) / 3
-    yc = (y_1 + y_2 + y_3) / 3
-    theta = math.atan2(2/3 * x_1 - 1/3 * (x_2 + x_3), 2/3 * y_1 - 1/3 * (y_2 + y_3))
-    return d1,d2,beta,xc,yc,theta
+def references(setpoint_):
+    setpoint_[0,0] = rospy.get_param('d1r',setpoint_[0,0])
+    setpoint_[1,0] = rospy.get_param('d2r',setpoint_[1,0])
+    setpoint_[2,0] = rospy.get_param('betar',setpoint_[2,0])
+    setpoint_[3,0] = rospy.get_param('xcr',setpoint_[3,0])
+    setpoint_[4,0] = rospy.get_param('ycr',setpoint_[4,0])
+    setpoint_[5,0] = rospy.get_param('thetar',setpoint_[5,0])
+    return setpoint_
+
+def controlFormation(x_1,y_1,x_2,y_2,x_3,y_3,theta_1,theta_2,theta_3, setpoint_, setpoint_ant_):
+    d_1 = math.sqrt((x_1 - x_3)**2 + (y_1 - y_3)**2)
+    d_2 = math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
+    d_3 = math.sqrt((x_2 - x_3)**2 + (y_2 - y_3)**2)
+    beta_ = math.acos((d_1**2 + d_2**2 - d_3**2) / (2 * d_1 * d_2))
+    xc_ = (x_1 + x_2 + x_3) / 3
+    yc_ = (y_1 + y_2 + y_3) / 3
+    theta_ = math.atan2(2/3 * x_1 - 1/3 * (x_2 + x_3), 2/3 * y_1 - 1/3 * (y_2 + y_3))
+    q = np.array([[d_1],[d_2],[beta_],[xc_],[yc_],[theta_]])
+    q_e = setpoint_ - q
+    q_d = (setpoint_ - setpoint_ant_) / 0.1
+    Jr = np.array([[math.cos(theta_1), -0.1 * math.sin(theta_1), 0.0 , 0.0 , 0.0 , 0.0], 
+                    [math.sin(theta_1), 0.1 * math.cos(theta_1), 0.0 , 0.0 , 0.0 , 0.0], 
+                    [ 0.0 , 0.0 , math.cos(theta_2), -0.1 * math.sin(theta_2), 0.0 , 0.0], 
+                    [ 0.0 , 0.0 , math.sin(theta_2), 0.1 * math.cos(theta_2), 0.0 , 0.0], 
+                    [ 0.0 , 0.0 , 0.0 , 0.0 , math.cos(theta_3), -0.1 * math.sin(theta_3)], 
+                    [ 0.0 , 0.0 , 0.0 , 0.0 , math.sin(theta_3), 0.1 * math.cos(theta_3)]])
+    J = np.array([[(2*x_1 - 2*x_3)/(2*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)), (2*y_1 - 2*y_3)/(2*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)), 0, 0, -(2*x_1 - 2*x_3)/(2*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)), -(2*y_1 - 2*y_3)/(2*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2))],
+                  [(2*x_1 - 2*x_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)), (2*y_1 - 2*y_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)), -(2*x_1 - 2*x_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)), -(2*y_1 - 2*y_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)), 0, 0],
+                  [((2*x_2 - 4*x_1 + 2*x_3)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) + ((2*x_1 - 2*x_2)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(3/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) + ((2*x_1 - 2*x_3)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(3/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2), ((2*y_2 - 4*y_1 + 2*y_3)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) + ((2*y_1 - 2*y_2)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(3/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) + ((2*y_1 - 2*y_3)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(3/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2), ((2*x_1 - 2*x_3)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) - ((2*x_1 - 2*x_2)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(3/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2), ((2*y_1 - 2*y_3)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) - ((2*y_1 - 2*y_2)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(3/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2), ((2*x_1 - 2*x_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) - ((2*x_1 - 2*x_3)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(3/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2), ((2*y_1 - 2*y_2)/(2*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(1/2)) - ((2*y_1 - 2*y_3)*((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2))/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)**(1/2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)**(3/2)))/(1 - ((x_1 - x_2)**2 + (x_1 - x_3)**2 - (x_2 - x_3)**2 + (y_1 - y_2)**2 + (y_1 - y_3)**2 - (y_2 - y_3)**2)**2/(4*((x_1 - x_2)**2 + (y_1 - y_2)**2)*((x_1 - x_3)**2 + (y_1 - y_3)**2)))**(1/2)],
+                  [1.0/3.0, 0, 1.0/3.0, 0, 1.0/3.0, 0],
+                  [0, 1.0/3.0, 0, 1.0/3.0, 0, 1.0/3.0],
+                  [-2/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)), (2*(x_2/3 - (2*x_1)/3 + x_3/3))/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)**2), 1/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)), -(x_2/3 - (2*x_1)/3 + x_3/3)/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)**2), 1/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)), -(x_2/3 - (2*x_1)/3 + x_3/3)/(3*((x_2/3 - (2*x_1)/3 + x_3/3)**2/(y_2/3 - (2*y_1)/3 + y_3/3)**2 + 1)*(y_2/3 - (2*y_1)/3 + y_3/3)**2)]
+                  ])
+    control_ = np.dot(np.linalg.inv(np.dot(J,Jr)),q_d + 0.6 * q_e)
+    return control_, setpoint_
 
 if __name__ == '__main__':
         #NODE DEFINITION
@@ -85,13 +112,18 @@ if __name__ == '__main__':
         diff_vel3 = rospy.Publisher('/cmd_vel3',Twist,queue_size=1)
         rate = rospy.Rate(10.0)
         while not rospy.is_shutdown():
-            #d1,d2,beta,xc,yc,theta = variablesFormation(x1,y1,x2,y2,x3,y3)
-            #var = np.array([[d1],[d2],[beta],[xc],[yc],[theta]])
-            print (1)
-            cmd = Twist()
-            cmd.linear.x = uref
-            cmd.angular.z = wref
-            diff_vel1.publish(cmd)
-            diff_vel2.publish(cmd)
-            diff_vel3.publish(cmd)
+            setpoint = references(setpoint)
+            control, setpoint_ant = controlFormation(x1,y1,x2,y2,x3,y3,theta1,theta2,theta3,setpoint,setpoint_ant)            
+            cmd1 = Twist()
+            cmd1.linear.x = control[0,0]
+            cmd1.angular.z = control[1,0]
+            cmd2 = Twist()
+            cmd2.linear.x = control[2,0]
+            cmd2.angular.z = control[3,0]
+            cmd3 = Twist()
+            cmd3.linear.x = control[4,0]
+            cmd3.angular.z = control[5,0]
+            diff_vel1.publish(cmd1)
+            diff_vel2.publish(cmd2)
+            diff_vel3.publish(cmd3)
             rate.sleep
